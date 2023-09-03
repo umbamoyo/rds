c42fcdb73ebac9abbbfe59a7fd446c81310644e2
Jyuka <yoyksks3@gmail.com>
2023-09-03 17:35:31 +0900

    공통 응답 포맷 및 에러코드 추가

---
+++ rds-api/src/main/java/com/example/rdsapi/constant/ErrorCode.java
@@ -0,0 +1,62 @@
+package com.example.rdsapi.constant;
+
+import com.example.rdsapi.exception.GeneralException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    OK(0, HttpStatus.OK, "Ok"),
+    BAD_REQUEST(10000, HttpStatus.BAD_REQUEST, "Bad request"),
+    VALIDATION_ERROR(10001, HttpStatus.BAD_REQUEST, "Validation error"),
+    EMAIL_OR_NICKNAME_EXIST(1002,HttpStatus.CONFLICT,"Duplicate Email or NickName"),
+    INTERNAL_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal error"),
+    ;
+
+    private final Integer code;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+
+    /**
+     * 예상하지 못한 에러가 발생한 경우 httpStatus 에 따라 ErrorCode 를 생성해주기 위함
+     * @param httpStatus
+     * @return
+     */
+    public static ErrorCode valueOf(HttpStatus httpStatus) {
+        if (httpStatus == null) { throw new GeneralException("HttpStatus is null."); }
+
+        // ErrorCode 전체를 조회하여 httpStatus 와 동일한 값을 탐색
+        // 존재하지 않으면 3xx, 4xx 에 맞는 에러코드 리턴
+        // 3xx, 4xx 가 아니면 200 리턴
+        return Arrays.stream(values())
+                .filter(errorCode -> errorCode.getHttpStatus() == httpStatus)
+                .findFirst()
+                .orElseGet(() -> {
+                    if (httpStatus.is4xxClientError()) { return ErrorCode.BAD_REQUEST; }
+                    else if (httpStatus.is5xxServerError()) { return ErrorCode.INTERNAL_ERROR; }
+                    else { return ErrorCode.OK; }
+                });
+    }
+
+    public String getMessage(Throwable e) {
+        return this.getMessage(this.getMessage() + " - " + e.getMessage());
+    }
+
+    public String getMessage(String message) {
+        return Optional.ofNullable(message)
+                .filter(Predicate.not(String::isBlank))
+                .orElse(this.getMessage());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%d)", this.name(), this.getCode());
+    }
+}
---
+++ rds-api/src/main/java/com/example/rdsapi/dto/response/common/ApiDataResponse.java
@@ -0,0 +1,55 @@
+package com.example.rdsapi.dto.response.common;
+
+
+import com.example.rdsapi.constant.ErrorCode;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiDataResponse<T> extends ApiErrorResponse {
+
+    private final T data;
+
+    private ApiDataResponse(T data) {
+        super(true, ErrorCode.OK.getCode(), ErrorCode.OK.getMessage());
+        this.data = data;
+    }
+    private ApiDataResponse(Integer errorCode, String customMessage, T data) {
+        super(true, errorCode, customMessage);
+        this.data = data;
+    }
+
+    public static <T> ApiDataResponse<T> of(T data) {
+        return new ApiDataResponse<>(data);
+    }
+    public static <T> ApiDataResponse<T> of(Integer errorCode, String customMessage, T data) {
+        return new ApiDataResponse<>(errorCode,customMessage,data);
+    }
+
+    public static <T> ApiDataResponse<T> empty() {
+        return new ApiDataResponse<>(null);
+    }
+
+    // 단순 성공 결과값 리턴시
+    public static ApiDataResponse<GeneralResponse> success(){
+        return new ApiDataResponse<>(new GeneralResponse(true));
+    }
+
+    // 단순 실패 결과값 리턴시
+    public static ApiDataResponse<GeneralResponse> failed(){
+        return new ApiDataResponse<>(new GeneralResponse(false));
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class GeneralResponse{
+        private boolean success;
+    }
+
+}
