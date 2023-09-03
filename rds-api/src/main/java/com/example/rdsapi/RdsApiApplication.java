dc9577e8269375bcbf5aec1fc194a779cff81083
Jyuka <yoyksks3@gmail.com>
2023-09-03 17:29:20 +0900

    Multi-Module 설정 : DB 연동 테스트

--- rds-api/src/main/java/com/example/rdsapi/RdsApiApplication.java
+++ rds-api/src/main/java/com/example/rdsapi/RdsApiApplication.java
@@ -2,12 +2,15 @@ package com.example.rdsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(
 		// 빈 스캔의 경우 해당 클래스가 위치한 패키지 하위를 스캔, common 모듈의 경우 패키지 경로가 다르기 때문에 추가
 		scanBasePackages = {"com.example.rdsapi", "com.example.rdscommon"}
-
 )
+@EntityScan("com.example.rdscommon.domain")
+@EnableJpaRepositories(basePackages = "com.example.rdscommon.repository")
 public class RdsApiApplication {
 
 	public static void main(String[] args) {
