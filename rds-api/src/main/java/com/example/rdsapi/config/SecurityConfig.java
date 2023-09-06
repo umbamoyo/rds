9940585a636f6ad253443c06a630024edbf8b63c
Jyuka <yoyksks3@gmail.com>
2023-09-06 17:21:08 +0900

    리액트 cors 관련 문제 임시조치

---
+++ rds-api/src/main/java/com/example/rdsapi/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.rdsapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors().and()
+                .authorizeRequests(auth -> auth
+                        .mvcMatchers("/api/v1/**").permitAll()
+                )
+                .csrf().disable()
+                .build();
+    }
+
+
+    // CORS 관련 설정
+    @Bean
+    public CorsConfigurationSource configurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",config);
+
+        return source;
+    }
+
+}
