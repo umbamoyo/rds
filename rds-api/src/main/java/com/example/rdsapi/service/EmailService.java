0f7b8e752c54ab8f1683e010d24f291dc758a7db
sdj799 <sdj799@naver.com>
2023-09-07 20:35:44 +0900

    09.07. 이메일 인증까지 진행

---
+++ rds-api/src/main/java/com/example/rdsapi/service/EmailService.java
@@ -0,0 +1,69 @@
+package com.example.rdsapi.service;
+
+import java.util.Random;
+
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    JavaMailSender emailSender;
+
+    public static final int ePw = makeRandomNumber();
+
+    private MimeMessage createMessage(String to)throws Exception{
+        System.out.println("보내는 대상 : "+ to);
+        System.out.println("인증 번호 : "+ePw);
+        MimeMessage  message = emailSender.createMimeMessage();
+
+        message.addRecipients(RecipientType.TO, to);//보내는 대상
+        message.setSubject("이메일 인증 테스트");//제목
+
+        String msgg="";
+        msgg+= "<div style='margin:20px;'>";
+        msgg+= "홈페이지를 방문해 주셔서 감사합니다.";
+        msgg+= "<br>";
+        msgg+= "<p>아래 코드를 복사해 입력해주세요<p>";
+        msgg+= "<br>";
+        msgg+= "<p>감사합니다.<p>";
+        msgg+= "<br>";
+        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
+        msgg+= "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
+        msgg+= "<div style='font-size:130%'>";
+        msgg+= "CODE : <strong>";
+        msgg+= ePw+"</strong><div><br/> ";
+        msgg+= "</div>";
+        message.setText(msgg, "utf-8", "html");//내용
+        message.setFrom(new InternetAddress("sdj799@gmail.com","DongjunShin"));//보내는 사람
+
+        return message;
+    }
+
+    //난수 발생
+    public static int makeRandomNumber() {
+        //난수의 범위: 111111 ~ 999999
+        Random r = new Random();
+        int checkNum = r.nextInt(888888) + 111111;
+        System.out.println("checkNum = " + checkNum);
+        return checkNum;
+    }
+
+    public int sendSimpleMessage(String to)throws Exception {
+        // TODO Auto-generated method stub
+        MimeMessage message = createMessage(to);
+        try{//예외처리
+            emailSender.send(message);
+        }catch(MailException es){
+            es.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+        return ePw;
+    }
+}
